65e5b05a0389f14f157556d95138b31f1c9c0aee
Serhiy Bardysh <sbardysh@tutanota.com>
2020-04-25 20:22:04 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,16 @@
 def fizzbuzz
   if int % 3 == 0
     "Fizz"
+  elseif int % 5 == 0
+    "Buzz"
+  elsif int % 15 == 0
+    "FizzBuzz"
+  else 
+      
+
   end
 end
 
 fizzbuzz(3)
 fizzbuzz(5)
-fizzbuzz() 
+fizzbuzz(15) 
